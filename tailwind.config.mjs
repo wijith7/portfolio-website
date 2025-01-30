4cbd62af10b13254d288d734b190f4e250c2bfc3
wijith <wijith7@gmail.com>
2025-01-30 12:27:27 +1030

    Create navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { Outfit } from 'next/font/google';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,9 +10,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover:'#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F'
       },
+      fontFamily:{
+        Outfit: ["Outfit","sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
     },
   },
   plugins: [],
