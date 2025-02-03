87503423eae8b549871e00236239c1a88b1dc792
wijith <wijith7@gmail.com>
2025-02-03 16:17:33 +1030

    create Footer section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
     <Services/>
     <Work/>
     <Contact/>
+    <Footer/>
     </>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,31 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-20 '>
+       <div className='text-center'>
+        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+
+    <div className='w-max flex items-center gap-2 mx-auto'>
+    <Image src={assets.mail_icon} alt='' className='w-6'/>
+    wijith7@gmail.com
+    </div>
+
+       </div>
+
+    <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+        <p>© 2025 wijith pathiranage. All rights reserved.</p>
+        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+            <li><a target='_blank' href="https://github.com/wijith7">GitHub</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/wijith-pathiranage-5034ab106/">LinkedIn</a></li>
+        </ul>
+    </div>
+
+    </div>
+
+  )
+}
+
+export default Footer
