024f690e4b8666169ad30a898eced2e638961c5f
wijith <wijith7@gmail.com>
2025-02-05 13:31:21 +1030

    add animations

--- components/About.jsx
+++ components/About.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, infoList, toolsData } from '@/assets/assets'
+import { motion } from "motion/react"
 
 const about = ({isDarkMode}) => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-      <h2 className='text-center text-5xl font-Ovo'>About me</h2>
+    <motion.div id='about' className='w-full px-[12%] py-10 scroll-mt-20'
+    
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+    >
+      <motion.h4
 
-        <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+      initial={{opacity:0, y: -20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.3}}
+      
+      className='text-center mb-2 text-lg font-Ovo'>Introduction</motion.h4>
+      <motion.h2
+      
+      initial={{opacity:0, y: -20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.5}}
+
+      className='text-center text-5xl font-Ovo'>About me</motion.h2>
+
+        <motion.div
+        
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:0.8}}
+        
+        
+        className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+            < motion.div
+            
+          initial={{opacity:0, scale:0.9}}
+          whileInView={{opacity:1, scale:1}}
+          transition={{duration:0.6}}
+            
+            
+            className='w-64 sm:w-80 rounded-3xl max-w-none'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
-            </div>
-            <div className='flex-1'>
+            </motion.div>
+            <motion.div
+            
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            transition={{duration:0.6, delay:0.8}}
+            
+            
+            className='flex-1'>
               <p className='mb-10 max-w-21xl font-Ovo'>
               A skilled software engineer with over two years of experience at WSO2, 
               contributing to the APIM Team, Identity and Access Management Team, and Rapid Response Team. 
@@ -22,10 +61,21 @@ const about = ({isDarkMode}) => {
               Experienced in React.js, JavaScript, Jenkins, Shell scripting, and MySQL.
               </p>
 
-              <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+              <motion.ul
+              
+              initial={{opacity:0}}
+              whileInView={{opacity:1}}
+              transition={{duration:0.8, delay:1}}
+              
+              
+              className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                 {infoList.map(({icon,iconDark, title,description},
                   index)=>(
-                  <li className='border-[0.5px] border-gray-400 
+                  <motion.li
+              
+                  whileHover={{scale:1.05}}
+                  
+                  className='border-[0.5px] border-gray-400 
                   rounded-xl p-6 cursor-pointer hover:bg-lightHover 
                   hover:-translate-y-1 duration-500 
                   hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
@@ -34,26 +84,44 @@ const about = ({isDarkMode}) => {
                     <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>
                       {title}</h3>
                     <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
-                  </li>
+                  </motion.li>
                 ))}
-              </ul>
+              </motion.ul>
 
-              <h4 className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</h4>
+              <motion.h4
+              
+              initial={{opacity:0, y: 20}}
+              whileInView={{opacity:1, y:0}}
+              transition={{duration:1.3, delay:0.5}}
+              
+              
+              className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</motion.h4>
 
-              <ul className='flex items-center gap-3 sm:gap-5'>
+              <motion.ul
+              
+              initial={{opacity:0}}
+              whileInView={{opacity:1}}
+              transition={{duration:0.6, delay:1.5}}
+              
+              
+              className='flex items-center gap-3 sm:gap-5'>
                 {toolsData.map((tool, index)=>(
-                  <li className='flex items-center justify-center 
+                  <motion.li
+                  
+                  whileHover={{scale:1.1}}
+                  
+                  className='flex items-center justify-center 
                   w-12 sm:w-14 aspect-square border border-gray-400
                   rounded-lg cursor-pointer hover:-translate-y-1 duration-500 ' 
                    key={index}>
                       <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
-                  </li>
+                  </motion.li>
                   ))}
-              </ul>
+              </motion.ul>
 
-            </div>
-        </div>
-    </div>
+            </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
 
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,20 +1,56 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Services = () => {
   return (
-    <div id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>What I offer</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+    <motion.div
 
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+    
+    id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
+
+      <motion.h4
+      
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{duration:0.3, delay:0.5}}
+      
+      
+      className='text-center mb-2 text-lg font-Ovo'>What I offer</motion.h4>
+
+      <motion.h2
+      
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{duration:0.5, delay:0.5}}
+      
+      className='text-center text-5xl font-Ovo'>My Services</motion.h2>
+
+      <motion.p
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{duration:0.5, delay:0.7}}
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+
       >Now transitioning into data analytics, data engineering, 
-        and AI engineering, combining software development expertise with data-driven insights.</p>
+        and AI engineering, combining software development expertise with data-driven insights.</motion.p>
 
-        <div className='grid grid-cols-auto gap-6 my-10'>
+        <motion.div
+        
+        initial={{opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{duration:0.6, delay:0.9}}
+        className='grid grid-cols-auto gap-6 my-10'>
             {serviceData.map(({icon,title,description,link}, index)=>(
-             <div key={index}
+
+             <motion.div
+             whileFocus={{scale:1.05}}
+             
+             key={index}
              className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover
               hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
                 <Image src={icon} alt='' className='w-10'/>
@@ -25,12 +61,12 @@ const Services = () => {
                  <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                     Read more <Image src={assets.right_arrow} alt='' className='w-4' />
                  </a>
-             </div>
+             </motion.div>
 
             ))}
 
-        </div>
-    </div>
+        </motion.div>
+    </motion.div>
   )
 }
 
