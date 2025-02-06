9eb9f0461c89b1cf9600a6901efc4e663298eb29
wijith <wijith7@gmail.com>
2025-02-06 17:39:40 +1030

    add git page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    output: 'export', // Tells Next.js to export the app as static files
+    trailingSlash: true, // Optional: Ensures URLs end with a slash, which is required for GitHub Pages
+  };
+  
