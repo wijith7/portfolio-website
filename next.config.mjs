1f2bc0bcfa57e443a1097acaea7bd3a5591aefcd
wijith <wijith7@gmail.com>
2025-02-07 14:55:53 +1030

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+//** @type {import('next').NextConfig} */
+//const nextConfig = {};
 
-export default nextConfig;
+//export default nextConfig;
+
+const nextConfig = {
+    output: 'export', // Enables static export
+    //basePath: '/portfolio-website', // Replace with your repository name
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
+  };
+  
+  export default nextConfig;
