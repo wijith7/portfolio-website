b567e54ccfabddf2353a3222469590002ddad1e1
wijith <wijith7@gmail.com>
2025-02-07 12:20:45 +1030

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-export default {
-    output: 'export',
-    basePath: '/portfolio-website',
-    assetPrefix: '/portfolio-website/',
-    trailingSlash: true, // Recommended for GitHub Pages
-    images: {
-      unoptimized: true, // Fix for Image Optimization error
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
