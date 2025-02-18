db849cfb27f68965c05d51440a441ce0cd58028d
wijith <wijith7@gmail.com>
2025-02-18 16:27:26 +1030

    Update resume

--- components/Header.jsx
+++ components/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
             whileInView={{y: 0, opacity: 1}}
             transition={{duration:0.6, delay:1.2}}
 
-            href="/resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark: text-black'
+            href="/Resume_Wijith_Pathiranage.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark: text-black'
             >my resume <Image src={assets.download_icon} alt='' className='w-4'/> </motion.a>
         </div>
 
